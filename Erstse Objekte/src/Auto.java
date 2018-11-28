5c4fb8b6783471f5fb42f62e5e6f40d7c574acfc
Neungang <romaineneungang@yahoo.fr>
2018-11-28 21:47:52 +0100

    abend

--- Erstse Objekte/src/Auto.java
+++ Erstse Objekte/src/Auto.java
@@ -10,7 +10,8 @@ public class Auto {
 	private double tankVolumen = 60;
 	private double verbrauch = 8;
 	private double reichweite;
-	String passwort = "Passwort";
+	private String passwort = "Passwort";
+	double tankstand = tankVolumen;
 
 	// Methoden = Fähigkeiten
 
@@ -40,14 +41,15 @@ public class Auto {
 			this.hubRaum = hubRaum;
 		else
 			this.hubRaum = 0;
+		this.tankstand = this.tankVolumen;
 
 	}
 
-	void fahre() {
-		// nimmt Strecke in Kilometer an
-		// Berechnet tankstand neu
-		// Nutzen sie dazu den verbrauch
-	}
+//	void fahre() {
+//		// nimmt Strecke in Kilometer an
+//		// Berechnet tankstand neu
+//		// Nutzen sie dazu den verbrauch
+//	}
 
 	void berechneReichweite() {
 		// Tank 100l
@@ -99,9 +101,11 @@ public class Auto {
 	void druckeSerienNummer() {
 		System.out.println(this.serienNummer);
 	}
+
 	void setKW(int kw) {
 		this.kw = kw;
 	}
+
 	int getKw() {
 		return this.kw;
 	}
@@ -109,16 +113,55 @@ public class Auto {
 	String getSerienNummer() {
 		return this.serienNummer;
 	}
-	
+
 	int getAnzahlSitze() {
 		return this.anzahlSitze;
 	}
+
 	// Fehler auslesen
 	String fehlerAuslesen(String passwort) {
-		//Wenn this.passwort = = Passwort
-		if(this.passwort == passwort)
+		// Wenn this.passwort = = Passwort
+		if (this.passwort == passwort)
 			return "Keine Fehler";
 		else
 			return "Passwort falsch!";
 	}
+	// Methode fahre()
+
+	// nimmt Strecke in Kilometer an
+
+	// Berechnet tankstand neu
+
+	// Nutzen sie dazu den verbrauch
+	void fahre(double strecke) {
+		double verbrauchneu = this.tankstand - this.verbrauch * strecke / 100;
+		
+		if (verbrauchneu >= 0) {
+			System.out.println("Sie können fahren!");		
+			this.tankstand -= this.verbrauch * strecke / 100;
+		}else {
+			System.out.println("ist leider nicht möglich!");
+		}
+	}
+
+	// Methode tanke()
+
+	// nimmt eine Anzahl an litern
+
+	// fügt sie dem Tankstand hinzu
+
+	// beachten sie auch das Tankvolumen
+	void tank(double anzahlLiter) {
+		if ((tankstand + anzahlLiter) <= tankVolumen) {
+			System.out.println("Sie können " + anzahlLiter + " Liter tanken!");
+//			tankstand = tankstand + anzahlLiter;
+			this.tankstand += anzahlLiter;
+		}
+		else {
+			System.out.println("Sorry! Sie können nur " + (tankVolumen - tankstand) + " Liter tanken!");
+			this.tankstand = tankVolumen;
+		}
+	}
+
+
 }
