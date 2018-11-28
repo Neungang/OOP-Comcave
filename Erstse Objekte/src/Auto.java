dd986473b2086eaae600bae93ddb36081cbd9e79
Neungang <romaineneungang@yahoo.fr>
2018-11-28 12:38:42 +0100

    Tag-5

--- Erstse Objekte/src/Auto.java
+++ Erstse Objekte/src/Auto.java
@@ -1,11 +1,115 @@
 
 public class Auto {
-	// Attribute	= 	Zustand
-	String marke;
-	String modell;
-	String serienNummer;
-	int kw;
-	int hubRaum;
+	// Attribute = Eigenschaften
+	private String marke; // der Modifizierer
+	private String modell;
+	private String serienNummer;
+	private int kw;
+	private int hubRaum;
+	private int anzahlSitze;
+	private double tankVolumen = 60;
+	private double verbrauch = 8;
+	private double reichweite;
+
+	// Methoden = Fähigkeiten
+
+	// void = kein Rückgabewert
+	void druckeMarkeUndModell() {
+		System.out.println(marke + " " + modell);
+		System.out.println(serienNummer);
+	}
+
+	// In die Klammern() schreiben wir Parameter
+	// Parameter müssen beim Aufruf der Funktion übergeben werden
+	// und sind nur in der Methode verfügbar
+	void setzeAttribute(String marke, String modell, String serienNummer, double tankVolumen, double verbrauch, int kw,
+			int hubRaum, int anzahlSitze) {
+
+		// Parameter MÜSSEN übergeben werden, wenn sie erwatet werden
+
+		this.modell = modell;
+		this.marke = marke;
+		this.serienNummer = serienNummer;
+		this.tankVolumen = tankVolumen;
+		this.verbrauch = verbrauch;
+		this.kw = kw;
+		if (anzahlSitze >= 2 && anzahlSitze <= 9)
+			this.anzahlSitze = anzahlSitze;
+		if (hubRaum > 0)
+			this.hubRaum = hubRaum;
+		else
+			this.hubRaum = 0;
+
+	}
+
+	void fahre() {
+		// nimmt Strecke in Kilometer an
+		// Berechnet tankstand neu
+		// Nutzen sie dazu den verbrauch
+	}
+
+	void berechneReichweite() {
+		// Tank 100l
+		// Verbrauch 10l / 100km
+		// Reichweite => 100l / 10l = 10 10*100km = 1000km
+
+		// Tank 60
+		// Verbrauch 5l / 100km
+		// Reichweite => 60l / 5l = 12 12*100km = 1200km
+
+		reichweite = tankVolumen / verbrauch * 100;
+	}
+
+	void druckeReichweite() {
+		System.out.println("Reichweite: " + reichweite + " km");
+	}
+
+	void druckeMarke() {
+		System.out.println("Marke: " + this.marke);
+	}
+
+	void druckeModell() {
+		System.out.println("Model: " + this.modell);
+	}
+
+	void druckeTankVolumen() {
+		System.out.println("Tank Volume: " + this.tankVolumen);
+	}
+
+	void druckeVerbrauch() {
+		System.out.println("Verbrauch: " + this.verbrauch);
+	}
+
+	void druckeKw() {
+		System.out.println("KW: " + this.kw);
+	}
+
+//	void druckeInfo() {
+//		druckeMarkeUndModell();
+//		berechneReichweite();
+//		System.out.println("Anzahl Sitze: " + this.anzahlSitze);
+//		System.out.println("Leistung: " + " kw");
+//		System.out.println("Hubraum: " + this.hubRaum + " cm3");
+//		System.out.println("Tankvolumen: " + this.tankVolumen + " l");
+//		System.out.println("Verbrauch: " + this.verbrauch + "l/100 km");
+//		druckeReichweite();
+//		System.out.println();
+//	}
+	void druckeSerienNummer() {
+		System.out.println(this.serienNummer);
+	}
+	void setKW(int kw) {
+		this.kw = kw;
+	}
+	int getKw() {
+		return this.kw;
+	}
+
+	String getSerienNummer() {
+		return this.serienNummer;
+	}
 	
-	// Methoden		=	Fähigkeiten
+	int getAnzahlSitze() {
+		return this.anzahlSitze;
+	}
 }
--- Erstse Objekte/src/Main.java
+++ Erstse Objekte/src/Main.java
@@ -3,23 +3,31 @@ public class Main {
 
 	public static void main(String[] args) {
 		//Scanner derScanner = new Scanner(System.in);
-		Auto Auto1 = new Auto();
-		Auto Auto2 = new Auto();
-		Auto Auto3 = new Auto();
+		Auto auto1 = new Auto();
+		Auto auto2 = new Auto();
+		Auto auto3 = new Auto();
+		Auto auto4 = new Auto();
 		
-		Auto1.marke = "Audi";
-		Auto1.modell = "A6";
-		Auto2.marke = "Mercedes";
-		Auto2.modell = "C-Klasse";
-		Auto3.marke = "BMW";
-		Auto3.modell = "3er";
+//		auto1.marke = "Audi";
+//		auto1.modell = "A6";
+//		auto2.marke = "Mercedes";
+//		auto2.modell = "C-Klasse";
+//		auto3.marke = "BMW";
+//		auto3.modell = "3er";
 		
-		Auto1.serienNummer = "734jsdfmd93";
+		auto1.setzeAttribute("Audi", "A6", "BSA465758", 72, 11, 160, 2000,5);
+		auto2.setzeAttribute("Mercedes", "E-Klasse", "SZw083475", 68, 9, 180, 3000,7);
+		auto3.setzeAttribute("BMW", "4er", "HIT74r6478", 72, 8, 160, 2000, 4);
+		auto1.druckeSerienNummer();
+		System.out.println("Die Seriennummer lautet " + auto1.getSerienNummer());
+		System.out.println("WIR HABEN INSGESAMT " + (auto1.getAnzahlSitze() + auto2.getAnzahlSitze() + auto3.getAnzahlSitze()) + " Plätze");
 		
-		System.out.println(Auto1.marke + " " + Auto1.modell);
-		System.out.println(Auto2.marke + " " + Auto2.modell);
-		System.out.println(Auto3.marke + " " + Auto3.modell);
-		
+		// Auto1 KW ausgeben
+		System.out.println(auto1.getKw());
+		// Auto1 KW ändern
+		auto1.setKW(150);
+		// Auto1 KW wieder ausgeben
+		System.out.println(auto1.getKw());
 		
 	}
 
